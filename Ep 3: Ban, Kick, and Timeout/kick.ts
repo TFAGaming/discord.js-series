ff56428645c0144549a3a223dd549df6a8a4ae30
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-06-09 00:39:54 +0100

    Add files via upload

--- Ep 3: Ban, Kick, and Timeout/kick.ts
+++ Ep 3: Ban, Kick, and Timeout/kick.ts
@@ -3,16 +3,16 @@ import { client } from "../..";
 
 export default new client.command({
     structure: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Ban a guild member.')
+        .setName('kick')
+        .setDescription('Kick a guild member.')
         .addUserOption((opt) =>
             opt.setName('user')
-                .setDescription('The user to ban.')
+                .setDescription('The user to kick.')
                 .setRequired(true)
         )
         .addStringOption((opt) =>
             opt.setName('reason')
-                .setDescription('The reason of the ban.')
+                .setDescription('The reason of the kick.')
                 .setRequired(false)
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
@@ -29,7 +29,9 @@ export default new client.command({
             return;
         };
 
-        if (!interaction.guild?.members.cache.get(user.id)) {
+        const member = interaction.guild?.members.cache.get(user.id);
+
+        if (!member) {
             await interaction.reply({
                 content: 'This user is not on the server!'
             });
@@ -38,14 +40,14 @@ export default new client.command({
         };
 
         try {
-            await interaction.guild.members.ban(user.id, { reason: reason });
+            await member.kick(reason);
 
             await interaction.reply({
-                content: `\`✅\` <@${user.id}> has been banned from the server!\n> ${reason}`
+                content: `\`✅\` <@${user.id}> has been kicked from the server!\n> ${reason}`
             });
         } catch {
             await interaction.reply({
-                content: `\`❌\` Unable to ban <@${user.id}>.`
+                content: `\`❌\` Unable to kick <@${user.id}>.`
             });
         };
     }
