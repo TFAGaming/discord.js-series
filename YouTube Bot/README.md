d3b95e4e76e90471298f99dcba366c90dbdb7de1
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-07-31 13:23:53 +0100

    Delete YouTube Bot directory

--- YouTube Bot/README.md
+++
@@ -1,5 +0,0 @@
-The entire source code of the bot.
-
-> Make sure to rename `.env.example` to  `.env`!
-
-© T.F.A 7524
--- YouTube Bot/src/events/Client/ready.ts
+++
@@ -1,5 +0,0 @@
-import { client } from "../..";
-
-client.once('ready', () => {
-    console.log('Logged in as: ' + client.user?.tag);
-});
